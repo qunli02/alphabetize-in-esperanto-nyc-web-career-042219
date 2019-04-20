c110fb95f8c00ee316db893f1e7a31733da82cb8
Qun Li Huang <qunli02@gmail.com>
2019-04-20 16:10:05 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,5 @@
 def alphabetize(arr)
   # code here
-  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   sentences_array.sort_by do |arr|
     arr.split("").map do |character|
       ESPERANTO_ALPHABET.index(character)
